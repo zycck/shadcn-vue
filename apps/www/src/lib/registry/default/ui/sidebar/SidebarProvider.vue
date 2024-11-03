ff1b5f0a1bf742a159b30223e494f4a91c0f889a
Hookin. <45350455+LuckyHookin@users.noreply.github.com>
2024-11-03 17:11:46 +0800

    fix(Sidebar): use `open.value` when setting the cookie (#862)

--- apps/www/src/lib/registry/default/ui/sidebar/SidebarProvider.vue
+++ apps/www/src/lib/registry/default/ui/sidebar/SidebarProvider.vue
@@ -30,7 +30,7 @@ function setOpen(value: boolean) {
   open.value = value // emits('update:open', value)
 
   // This sets the cookie to keep the sidebar state.
-  document.cookie = `${SIDEBAR_COOKIE_NAME}=${open}; path=/; max-age=${SIDEBAR_COOKIE_MAX_AGE}`
+  document.cookie = `${SIDEBAR_COOKIE_NAME}=${open.value}; path=/; max-age=${SIDEBAR_COOKIE_MAX_AGE}`
 }
 
 function setOpenMobile(value: boolean) {
